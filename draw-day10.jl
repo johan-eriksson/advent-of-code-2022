1d47099cd0ddf873ae78ba3b5790793e9e44d788
Johan Eriksson <imoxus@gmail.com>
2022-12-13 18:00:42 +0900

    Day 10

---
+++ draw-day10.jl
@@ -0,0 +1,62 @@
+# %%
+using Test
+
+# %%
+f = readlines("day10.input");
+
+# %%
+mutable struct Cpu
+    score::Int64
+    cycle::Int64
+    x::Int64 
+    Cpu() = new(0, 1, 1)
+end
+
+
+# %%
+function increment_cycle(cpu::Cpu)
+
+    print(abs((cpu.x + 1) - cpu.cycle%40) < 2 ? "X" : " ")
+    if cpu.cycle % 40 == 0
+        print('\n')
+    end
+
+    cpu.cycle += 1
+    if (20+cpu.cycle)%40 == 0 && cpu.cycle < 271
+        cpu.score += cpu.cycle*cpu.x
+    end
+end
+
+# %%
+function noop(cpu::Cpu)
+    increment_cycle(cpu)
+end
+
+# %%
+function addx(cpu::Cpu, val)
+    increment_cycle(cpu)
+    increment_cycle(cpu)
+    cpu.x += val
+end
+
+# %%
+function run(cpu::Cpu, instructions)
+    for instruction in instructions
+        if instruction == "noop"
+            noop(cpu)
+        else
+            addx(cpu, parse(Int64, last(split(instruction))))
+        end
+    end
+end
+
+# %%
+function solve_part_1(input)
+    cpu = Cpu()
+    run(cpu, input)
+
+    cpu.score
+end 
+
+solve_part_1(f)
+
